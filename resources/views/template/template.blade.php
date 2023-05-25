bb183e80eaed3939b80dac6d6691fd1801103211
joshuajevon <joshuajevon05@gmail.com>
2023-05-25 10:31:55 +0700

    add: added template for laravel tailwind

---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasar Aset</title>
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/base/base.css') }}?t={{ env('VERSION_TIME') }}">
+    <link rel="stylesheet" href="{{ asset('css/base/build.css') }}?t={{ env('VERSION_TIME') }}">
+
+    <!-- vite-->
+    @vite('resources/css/app.css')
+
+    <!-- css & js-->
+    @yield('head')
+</head>
+
+@yield('body')
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('public/css/build.css')
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</body>
-</html>
+@extends('template.template')
+
+@section('head')
+    {{-- css --}}
+
+    <!-- javascript -->
+
+@endsection
+
+@section('body')
+    <h1 class="text-3xl font-bold underline">
+        Hello world Test!
+    </h1>
+@endsection
