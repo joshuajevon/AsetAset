df49b278a2d33f41a2e4c3212e958a221765edc6
joshuajevon <joshuajevon05@gmail.com>
2023-05-25 19:15:52 +0700

    rename proejct

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pasar Aset</title>
+    <title>Aset Aset</title>
     <link rel="shortcut icon" href="" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/base/base.css') }}?t={{ env('VERSION_TIME') }}">
     <link rel="stylesheet" href="{{ asset('css/base/build.css') }}?t={{ env('VERSION_TIME') }}">
