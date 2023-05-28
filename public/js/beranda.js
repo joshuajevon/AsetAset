88a4bd415b744511c2c56f79863f0ad2b62eb27c
TessyJr <christopher.nathanael1217@gmail.com>
2023-05-28 08:46:50 +0700

    [add]:add filter to home page

--- public/js/beranda.js
+++ public/js/beranda.js
@@ -1,6 +1,24 @@
 var topSplide = new Splide("#top-splide", {
-    type   : 'loop',
-    // cover: true,
-    heightRatio: 0.3,
+    autoplay: true,
+    drag: true,
+    type: "loop",
+    height: "400px",
+    breakpoints: {
+        640: {
+            height: "200px",
+        },
+        768: {
+            height: "250px",
+        },
+        1024: {
+            height: "300px",
+        },
+        1280: {
+            height: "350px",
+        },
+        1536: {
+            height: "400px",
+        },
+    },
 });
 topSplide.mount();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,28 +12,31 @@
     <x-navigation-bar page="beranda" />
 
     {{-- Carousel Top --}}
-    <section class="pt-20">
-        <div id="top-splide" class="splide" role="group">
-            <div class="splide__track">
-                <ul class="splide__list">
-                    <li class="splide__slide" alt="">
-                        <img class="w-full h-full object-contain"
-                            src="https://images.unsplash.com/photo-1679678691328-54929d271c3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                            alt="">
-                    </li>
-                    <li class="splide__slide"><img class="w-full h-full object-contain"
-                            src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1174&q=80"
-                            alt=""></li>
-                    <li class="splide__slide"><img class="w-full h-full object-contain"
-                            src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                            alt=""></li>
-                </ul>
+    <section class="pt-20 bg-cLightGrey bg-cover bg-center bg-[url('/public/assets/beranda/carousel-bg.png')]">
+        <div class="py-6 sm:py-8 md:py-10 lg:py-12 xl:py-14 2xl:py-16 px-12 sm:px-16 md:px-20 lg:px-24 xl:px-28 2xl:px-32">
+            <div id="top-splide" class="splide" role="group">
+                {{-- <ul class="splide__pagination"></ul> --}}
+                <div class="splide__track">
+                    <ul class="splide__list">
+                        <li class="splide__slide" alt="">
+                            <img class="w-full h-full object-contain"
+                                src="https://images.unsplash.com/photo-1679678691328-54929d271c3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                                alt="">
+                        </li>
+                        <li class="splide__slide"><img class="w-full h-full object-contain"
+                                src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1174&q=80"
+                                alt=""></li>
+                        <li class="splide__slide"><img class="w-full h-full object-contain"
+                                src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                                alt=""></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </section>
 
     {{-- Asets --}}
-    <section class="c-container py-8 lg:py-16 xl:py-32 flex flex-col gap-4 lg:gap-8 xl:gap-16">
+    <section class="c-container py-8 lg:py-16 xl:py-32 flex flex-col gap-8 lg:gap-12 xl:gap-16">
         {{-- Title --}}
         <div class="flex justify-center items-center gap-8">
             <h1 class="text-4xl font-bold text-cGold">Galeri Aset</h1>
@@ -44,7 +47,9 @@
         <div class="self-center w-full max-w-[800px] ">
             <form class="w-full gap-2">
                 <div class="py-3 px-6 flex rounded-full bg-cGold text-cWhite">
-                    <input class="w-full bg-transparent border-none placeholder:text-cWhite text-base active:border-none active:outline-none" id="search" name="search" placeholder="Pencarian...">
+                    <input
+                        class="w-full bg-transparent border-none placeholder:text-cWhite text-base active:border-none active:outline-none"
+                        id="search" name="search" placeholder="Pencarian...">
                     <button type="button" class="flex justify-center items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                             class="bi bi-search" viewBox="0 0 16 16">
@@ -57,38 +62,102 @@
         </div>
 
 
-        <div class="">
-            {{ $assets->links() }}
+        <div class="grid grid-cols-4 gap-8">
+            {{-- Filter --}}
+            <div class="flex flex-col bg-cWhite grow col-span-1 h-fit">
+                <div class="p-4 bg-cDarkGrey">
+                    <h1 class="font-bold text-center">Filter Pencarian</h1>
+                </div>
+
+                <div class="p-4 flex flex-col gap-2">
+                    <h2 class="font-bold">Jenis Aset</h2>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                </div>
+
+                <div class="p-4 flex flex-col gap-2">
+                    <h2 class="font-bold">Wilayah</h2>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                </div>
+
+                <div class="p-4 flex flex-col gap-2">
+                    <h2 class="font-bold">Rentang Harga</h2>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <input id="filter-apartemen" type="checkbox" class="appearance-none checked:bg-cGold" />
+                        <label for="filter-apartemen">Apartemen</label>
+                    </div>
+                </div>
+
+            </div>
+
+            {{-- Aset galeri --}}
+            <div class="flex flex-col gap-8 col-span-3">
+                <div class="">
+                    {{ $assets->links() }}
+                </div>
+
+                {{-- Items --}}
+                <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-3 lg:gap-4">
+                    @foreach ($assets as $asset)
+                        <a href="{{ route('asset-by-id', $asset->id) }}"
+                            class="group bg-cWhite border border-cDarkGrey p-2 sm:p-3 md:p-4 flex flex-col justify-center items-center gap-4">
+                            <div class="relative aspect-square flex justify-center items-center border border-cDarkGrey">
+                                <img src="{{ asset('/storage/asset/image/' . $asset->image) }}" class="object-contain "
+                                    alt="asset">
+                            </div>
+
+                            <div
+                                class="relative bg-white flex flex-col justify-center items-center gap-1 sm:gap-2 lg:gap-4">
+                                <p class="text-center text-xs sm:text-base">{{ $asset->name }}</p>
+                                <h3 class="text-center text-sm sm:text-lg lg:text-2xl text-cGold font-bold">
+                                    @currency ($asset->price)
+                                </h3>
+                                <div class="flex justify-center items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <p class="text-center text-xs sm:text-sm">{{ $asset->province }}</p>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
         </div>
 
-        {{-- Items --}}
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-            @foreach ($assets as $asset)
-                <a href="{{route('asset-by-id', $asset->id)}}"
-                    class="group bg-cWhite border border-cDarkGrey p-2 sm:p-3 md:p-4 flex flex-col justify-center items-center gap-4">
-                    <div class="relative aspect-square flex justify-center items-center border border-cDarkGrey">
-                        <img src="{{ asset('/storage/asset/image/' . $asset->image) }}"
-                            class="object-contain " alt="asset">
-                    </div>
 
-                    <div class="relative bg-white flex flex-col justify-center items-center gap-4">
-                        <p class="text-base">{{ $asset->name }}</p>
-                        <h3 class="text-xl text-cGold font-bold">
-                            @currency ($asset->price)
-                        </h3>
-                        <div class="flex justify-center items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-geo-alt" viewBox="0 0 16 16">
-                                <path
-                                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                            </svg>
-                            <p class="text-sm">{{ $asset->province }}</p>
-                        </div>
-                    </div>
-                </a>
-            @endforeach
-        </div>
     </section>
 
 
