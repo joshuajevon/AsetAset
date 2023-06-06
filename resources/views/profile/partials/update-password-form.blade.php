c10066e11e76e8d26db243a1ce28315ca0b826ed
TessyJr <christopher.nathanael1217@gmail.com>
2023-06-06 12:07:58 +0700

    fix: fix some stylings

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -5,29 +5,29 @@
         </h2>
 
         <p class="mt-1 text-sm sm:text-base text-gray-600 dark:text-gray-400">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+            {{ __('Pastikan password Anda terdiri dari 8 karakter, mengandung huruf besar, huruf kecil, dan angka') }}
         </p>
     </header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+    <form id="form-update-password" method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('put')
 
         <div>
             <x-input-label for="current_password" :value="__('Current Password')" />
-            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" />
+            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" placeholder="Masukkan password Anda" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="password" :value="__('New Password')" />
-            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" />
+            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" placeholder="Masukkan password baru Anda" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" />
+            <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" placeholder="Masukkan password baru Anda lagi" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
