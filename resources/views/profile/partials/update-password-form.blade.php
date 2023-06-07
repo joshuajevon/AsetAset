1991cc93dfea0104c1de6605e83573eaacf91146
TessyJr <christopher.nathanael1217@gmail.com>
2023-06-07 12:16:17 +0700

    fix: fix user dashboard

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -15,19 +15,22 @@
 
         <div>
             <x-input-label for="current_password" :value="__('Current Password')" />
-            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" placeholder="Masukkan password Anda" />
+            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full"
+                placeholder="Masukkan password Anda" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="password" :value="__('New Password')" />
-            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" placeholder="Masukkan password baru Anda" />
+            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
+                placeholder="Masukkan password baru Anda" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" placeholder="Masukkan password baru Anda lagi" />
+            <x-text-input id="password_confirmation" name="password_confirmation" type="password"
+                class="mt-1 block w-full" placeholder="Masukkan password baru Anda lagi" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
@@ -35,13 +38,8 @@
             <x-primary-button>{{ __('Simpan') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-green-600 font-bold">{{ __('Sukses.') }}</p>
             @endif
         </div>
     </form>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -19,27 +19,31 @@
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autocomplete="name" />
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
+                required autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
             <x-input-label for="nickname" :value="__('Nickname')" />
-            <x-text-input id="nickname" name="nickname" type="text" class="mt-1 block w-full" :value="old('nickname', $user->nickname)" required autocomplete="nickname" />
+            <x-text-input id="nickname" name="nickname" type="text" class="mt-1 block w-full" :value="old('nickname', $user->nickname)"
+                required autocomplete="nickname" />
             <x-input-error class="mt-2" :messages="$errors->get('nickname')" />
         </div>
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)"
+                required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
                         {{ __('Your email address is unverified.') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                        <button form="send-verification"
+                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                             {{ __('Click here to re-send the verification email.') }}
                         </button>
                     </p>
@@ -55,7 +59,8 @@
 
         <div>
             <x-input-label for="phone_number" :value="__('Phone Number')" />
-            <x-text-input id="phone_number" name="phone_number" type="text" class="mt-1 block w-full" :value="old('phone_number', $user->phone_number)" required autocomplete="phone_number" />
+            <x-text-input id="phone_number" name="phone_number" type="text" class="mt-1 block w-full"
+                :value="old('phone_number', $user->phone_number)" required autocomplete="phone_number" />
             <x-input-error class="mt-2" :messages="$errors->get('phone_number')" />
         </div>
 
@@ -65,12 +70,18 @@
             </div>
             <div class="mt-1 w-full flex justify-start items-center gap-8 text-sm sm:text-base">
                 <div class="flex justify-center items-center gap-2">
-                    <input class="cursor-pointer rounded-full appearance-none text-cGold focus:ring-0 focus:ring-offset-0" type="radio" id="laki-laki" name="gender" value="Laki-laki" @if ($user->gender == "Laki-laki") checked="checked" @endif>
+                    <input
+                        class="cursor-pointer rounded-full appearance-none text-cGold focus:ring-0 focus:ring-offset-0"
+                        type="radio" id="laki-laki" name="gender" value="Laki-laki"
+                        @if ($user->gender == 'Laki-laki') checked="checked" @endif>
                     <label for="laki-laki">Laki-laki</label>
                 </div>
 
                 <div class="flex justify-center items-center gap-2">
-                    <input class="cursor-pointer rounded-full appearance-none text-cGold focus:ring-0 focus:ring-offset-0" type="radio" id="perempuan" name="gender" value="Perempuan" @if ($user->gender == "Perempuan") checked="checked" @endif>
+                    <input
+                        class="cursor-pointer rounded-full appearance-none text-cGold focus:ring-0 focus:ring-offset-0"
+                        type="radio" id="perempuan" name="gender" value="Perempuan"
+                        @if ($user->gender == 'Perempuan') checked="checked" @endif>
                     <label for="perempuan">Perempuan</label>
                 </div>
             </div>
@@ -83,13 +94,8 @@
             <x-primary-button>{{ __('Simpan') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-green-600 font-bold">{{ __('Sukses.') }}</p>
             @endif
         </div>
     </form>
