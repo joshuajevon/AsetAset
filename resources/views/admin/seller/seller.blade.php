2bb97b340f6fc305f86b28dc58e07c139cab12ca
TessyJr <christopher.nathanael1217@gmail.com>
2023-06-08 21:40:42 +0700

    fix: fix admin panel manage seller stylings

--- resources/views/admin/seller/seller.blade.php
+++ resources/views/admin/seller/seller.blade.php
@@ -11,34 +11,56 @@
         <x-admin-navigation-bar page="manage-sellers" />
 
         <div
-            class="flex flex-col justify-center items-start gap-8 sm:gap-12 lg:gap-16 w-full c-container py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 2xl:py-14 ml-[72px] lg:ml-[18rem] mt-16">
+            class="flex flex-col justify-center items-start gap-8 w-full c-container py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 2xl:py-14 ml-[72px] lg:ml-[18rem] mt-16">
 
-            <h1 class="text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-cGold">Manage Sellers</h1>
+            <h1 class="text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-cGold">
+                Manage Sellers
+            </h1>
 
-            <a href="{{ route('create-seller') }}">Add</a>
+            <a class="gold-btn flex justify-center items-center gap-2" href="{{ route('create-seller') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                </svg>
 
-            <table style="border:1px solid black">
-                <th>
-                <td>name</td>
-                <td>action</td>
-                </th>
-                @foreach ($sellers as $seller)
+
+                Add</a>
+
+            <table class="w-full divide-y-2 divide-cGold bg-white text-sm border border-cGold table-auto">
+                <thead class="text-left text-base">
                     <tr>
-
-                        <td>{{ $seller->seller_name }}</td>
-                        <td>
-                            <a href="{{ route('view-seller', ['id' => $seller->id]) }}"><button type="submit"
-                                    class="btn btn-success">View</button></a>
-                            <a href="{{ route('edit-seller', ['id' => $seller->id]) }}"><button type="submit"
-                                    class="btn btn-success">Edit</button></a>
-                            <form action="{{ route('delete-seller', ['id' => $seller->id]) }}" method="post">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">
+                            Name
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">
+                            Action
+                        </th>
                     </tr>
-                @endforeach
+                </thead>
+
+                <tbody class="divide-y divide-cGold text-sm">
+                    @foreach ($sellers as $seller)
+                        <tr class="odd:bg-gray-100">
+                            <td class="whitespace-nowrap px-4 py-2">{{ $seller->seller_name }}</td>
+                            <td class="whitespace-nowrap px-4 py-2">
+                                <a class="bg-green-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
+                                    href="{{ route('view-seller', ['id' => $seller->id]) }}"><button
+                                        type="submit">View</button></a>
+                                <a class="bg-blue-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
+                                    href="{{ route('edit-seller', ['id' => $seller->id]) }}"><button
+                                        type="submit">Edit</button></a>
+                                <form class="inline" action="{{ route('delete-seller', ['id' => $seller->id]) }}"
+                                    method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit"
+                                        class="bg-red-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)]">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
             </table>
         </div>
     </div>
--- resources/views/admin/seller/view-seller.blade.php
+++ resources/views/admin/seller/view-seller.blade.php
@@ -11,32 +11,56 @@
         <x-admin-navigation-bar page="manage-sellers" />
 
         <div
-            class="flex flex-col justify-center items-start gap-8 sm:gap-12 lg:gap-16 w-full c-container py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 2xl:py-14 ml-[72px] lg:ml-[18rem] mt-16">
+            class="flex flex-col justify-center items-start gap-8 w-full c-container py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 2xl:py-14 ml-[72px] lg:ml-[18rem] mt-16">
+
+            <a href="{{ route('seller') }}" class="gold-btn flex justify-center items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                    class="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+
+                Kembali</a>
 
             <h1 class="text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-cGold">View Seller</h1>
 
-            <table style="border:1px solid black">
-                <th>
-                <td>name</td>
-                <td>address</td>
-                <td>phone</td>
-                <td>action</td>
-                </th>
-                <tr>
+            <table class="w-full divide-y-2 divide-cGold bg-white text-sm border border-cGold table-auto">
+                <thead class="text-left text-base">
+                    <tr>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">
+                            Name
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">
+                            Address
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">
+                            Phone Number
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
 
-                    <td>{{ $seller->seller_name }}</td>
-                    <td>{{ $seller->seller_address }}</td>
-                    <td>{{ $seller->seller_phone }}</td>
-                    <td>
-                        <a href="{{ route('edit-seller', ['id' => $seller->id]) }}"><button type="submit"
-                                class="btn btn-success">Edit</button></a>
-                        <form action="{{ route('delete-seller', ['id' => $seller->id]) }}" method="post">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
+                <tbody class="divide-y divide-cGold text-sm">
+                    <tr>
+                        <td class="whitespace-nowrap px-4 py-2">{{ $seller->seller_name }}</td>
+                        <td class="whitespace-nowrap px-4 py-2">{{ $seller->seller_address }}</td>
+                        <td class="whitespace-nowrap px-4 py-2">{{ $seller->seller_phone }}</td>
+                        <td class="whitespace-nowrap px-4 py-2">
+                            <a class="bg-blue-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
+                                href="{{ route('edit-seller', ['id' => $seller->id]) }}"><button
+                                    type="submit">Edit</button></a>
+                            <form class="inline" action="{{ route('delete-seller', ['id' => $seller->id]) }}"
+                                method="post">
+                                @csrf
+                                @method('delete')
+                                <button
+                                    class="bg-red-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)]"
+                                    type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
