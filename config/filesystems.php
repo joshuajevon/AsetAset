94a0152477e40448c4008aafafe827326267381e
joshuajevon <joshuajevon05@gmail.com>
2023-06-24 10:39:15 +0700

    add: permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,7 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
+            'permission' => 0755,
         ],
 
         's3' => [
