4c8f8fd35def40639b22ce158e36145dce54c56b
TessyJr <christopher.nathanael1217@gmail.com>
2023-06-26 09:44:29 +0700

    fix: fix footer and admin dashboard

--- resources/views/components/admin-navigation-bar.blade.php
+++ resources/views/components/admin-navigation-bar.blade.php
@@ -91,14 +91,15 @@
                 <span class="hidden lg:inline text-base font-medium"> Manage Slideshows </span>
             </a>
 
-            <a href="https://drive.google.com/"
+            <a href="https://drive.google.com/" target="_blank" rel="noopener noreferrer"
                 class="flex items-center gap-2.5 rounded-lg px-2 lg:px-4 py-2 lg:py-3 w-fit lg:w-full @if ($page == 'manage-slideshows') bg-gray-100 text-cGold @else text-cWhite hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] @endif">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+                        d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                 </svg>
-                <span class="hidden lg:inline text-base font-medium"> Admin Dashboard Guidebook </span>
+
+                <span class="hidden lg:inline text-base font-medium"> Admin Guidebook </span>
             </a>
 
             <div class="bg-cWhite h-0.5 w-full my-2"></div>
