39e9b3fbd047ae3f0681a19c76a4e8e4b4fc207e
TessyJr <christopher.nathanael1217@gmail.com>
2023-06-27 10:48:35 +0700

    fix: fix link

--- resources/views/aset-by-id.blade.php
+++ resources/views/aset-by-id.blade.php
@@ -220,7 +220,7 @@
                             kami</a>
                     @endguest
                     @auth
-                        <a href="https://wa.me/6287876731950?text=Mau%20beli%20batagor%20donk" target="_blank"
+                        <a href="https://wa.me/6287876731950?text=Halo%20asetaset.com,%20saya%20{{ Auth::user()->nickname }}%20berminat%20atas:%0D%0AAset:%20{{ $asset->name }}%0D%0ALink:%20https://asetaset.com/aset/{{ $asset->id }}%0D%0AMohon%20informasinya.%20Terima%20kasih." target="_blank"
                             rel="noopener noreferrer" class="gold-btn rounded-lg font-medium py-4 px-8">Anda berminat? Segera
                             hubungi kami</a>
                     @endauth
