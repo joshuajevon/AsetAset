9ae9c3ce8cb10896b86739b99404eec6d68881b7
TessyJr <christopher.nathanael1217@gmail.com>
2023-06-27 16:52:09 +0700

    fix: fix admin manage announcement stylings

--- resources/views/admin/announcement/announcement.blade.php
+++ resources/views/admin/announcement/announcement.blade.php
@@ -22,31 +22,34 @@
                 </svg>
                 Add
             </a>
-            <form action="{{ route('announcement') }}" method="GET">
-            {{-- Search Bar --}}
-            <div class="self-center w-full">
-                {{-- <form class="w-full gap-2 text-base"> --}}
-                    <div class="py-1 sm:py-2 lg:py-3 px-6 sm:px-7 lg:px-8 flex rounded-full bg-cGold text-cWhite">
-                        <input autocomplete="false" type="text"
-                            class="w-full bg-transparent border-none placeholder:text-cWhite px-0 autofill:shadow-[inset_0_0_0px_1000px_rgb(197,175,102)]"
-                            id="search" name="search" placeholder="Pencarian..." value="{{$result}}">
-                        <button type="submit" class="flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-search" viewBox="0 0 16 16">
-                                <path
-                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                            </svg>
-                        </button>
+
+            {{-- Search Bar, Sort, Refresh --}}
+            <form action="{{ route('announcement') }}" method="GET"
+                class="w-full flex flex-col justify-center items-start gap-8">
+                {{-- Search Bar --}}
+                <div class="self-center w-full">
+                    <div class="w-full gap-2 text-base">
+                        <div class="py-1 sm:py-2 lg:py-3 px-6 sm:px-7 lg:px-8 flex rounded-full bg-cGold text-cWhite">
+                            <input autocomplete="false" type="text"
+                                class="w-full bg-transparent border-none placeholder:text-cWhite px-0 autofill:shadow-[inset_0_0_0px_1000px_rgb(197,175,102)]"
+                                id="search" name="search" placeholder="Pencarian..." value="{{ $result }}">
+                            <button type="submit" class="flex justify-center items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                    class="bi bi-search" viewBox="0 0 16 16">
+                                    <path
+                                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
-                {{-- </form> --}}
-            </div>
+                </div>
 
 
-            {{-- Sort and refresh --}}
-            <div class="flex w-full gap-2">
-                {{-- Sorting --}}
-                <div class="flex justify-start items-center gap-2">
-                    <label class="hidden sm:block text-lg font-bold" for="sortOption">Urutkan:</label>
+                {{-- Sort and refresh --}}
+                <div class="flex w-full gap-2">
+                    {{-- Sorting --}}
+                    <div class="flex justify-start items-center gap-2">
+                        <label class="hidden sm:block text-lg font-bold" for="sortOption">Urutkan:</label>
 
                         <select class="cursor-pointer rounded-md" name="filter" id="filter"
                             onchange="this.form.submit()">
@@ -58,11 +61,9 @@
                             <option value="updated" {{ Request::query('filter') === 'updated' ? 'selected' : '' }}>
                                 Baru Update</option>
                         </select>
-                    </form>
-                </div>
+                    </div>
 
-                {{-- refresh --}}
-                <div>
+                    {{-- Refresh --}}
                     <a href="{{ route('announcement') }}"
                         class="flex justify-center items-center p-2 bg-cGold text-cWhite rounded-md transition hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)]">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
@@ -72,35 +73,36 @@
                         </svg>
                     </a>
                 </div>
-            </div>
+            </form>
 
-                <table class="w-full divide-y-2 divide-cGold bg-white text-sm border border-cGold table-auto">
-                    <thead class="text-left text-base">
-                        <tr>
-                            <th class="whitespace-nowrap px-4 py-2 font-medium">Title</th>
-                            <th class="whitespace-nowrap px-4 py-2 font-medium">Action</th>
+
+            <table class="w-full divide-y-2 divide-cGold bg-white text-sm border border-cGold table-auto">
+                <thead class="text-left text-base">
+                    <tr>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">Title</th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium">Action</th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-cGold text-sm">
+                    @foreach ($announcements as $announcement)
+                        <tr class="odd:bg-gray-100">
+                            <td class="whitespace-nowrap px-4 py-2">{{ $announcement->title }}</td>
+                            <td class="whitespace-nowrap px-4 py-2">
+                                <a class="bg-green-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
+                                    href="{{ route('view-announcement', ['id' => $announcement->id]) }}"><button
+                                        type="submit" class="btn btn-success">View</button></a>
+                                <a class="bg-blue-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
+                                    href="{{ route('edit-announcement', ['id' => $announcement->id]) }}"><button
+                                        type="submit" class="btn btn-success">Edit</button></a>
+                                <button onclick="deleteAnnouncement({{ $announcement->id }})"
+                                    class="bg-red-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)]">
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody class="divide-y divide-cGold text-sm">
-                        @foreach ($announcements as $announcement)
-                            <tr class="odd:bg-gray-100">
-                                <td class="whitespace-nowrap px-4 py-2">{{ $announcement->title }}</td>
-                                <td class="whitespace-nowrap px-4 py-2">
-                                    <a class="bg-green-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
-                                        href="{{ route('view-announcement', ['id' => $announcement->id]) }}"><button type="submit"
-                                            class="btn btn-success">View</button></a>
-                                    <a class="bg-blue-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)] mr-2"
-                                        href="{{ route('edit-announcement', ['id' => $announcement->id]) }}"><button type="submit"
-                                            class="btn btn-success">Edit</button></a>
-                                    <button onclick="deleteAnnouncement({{ $announcement->id }})"
-                                        class="bg-red-200 py-2 px-4 rounded-lg hover:bg-[linear-gradient(rgb(0_0_0/10%)_0_0)]">
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                    @endforeach
+                </tbody>
+            </table>
 
             {{-- Bottom Pagination --}}
             <div id="top-pagination" class="pagination">
