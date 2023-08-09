e24fbd861a62f96fbeae08f36564085193dceb0b
TessyJr <christopher.nathanael1217@gmail.com>
2023-08-09 20:32:28 +0700

    fix: fix favicon

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -11,7 +11,7 @@
 
     {{-- favicon --}}
     <link rel="icon" href="/favicon.ico" sizes="48x48">
-    <link rel="icon" href="/icon.svg" type="image/svg+xml">
+    {{-- <link rel="icon" href="/icon.svg" type="image/svg+xml"> --}}
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <link rel="manifest" href="/manifest.webmanifest">
 
